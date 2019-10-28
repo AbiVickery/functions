b4214bb16ef08f9517a37ab34e5b79ac6c8e7e0a
Abi Vickery <vickeryabi@gmail.com>
2019-10-28 10:40:58 -0500

    chili

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,16 @@
 // console.log('Hello World');
 
-function name(first,last) {
-    last = last ? : 'Curry';
+// function name(first, last = 'Vickery') {
+//     last = last ? : 'Curry'; //ternary
+//     console.log(first, last);
+// }
+
+function name(first = 'Abby', last = 'Curry') {
     console.log(first, last);
 }
 
-name('Abby likes','Curry');
+name('Abby', 'Chili');
+name('Abby');
+name();
 
-if(last) { last } else { 'Curry' };
+// if(last) { last } else { 'Curry' };
